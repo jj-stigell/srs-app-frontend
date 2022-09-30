57eafbbd7380b2b112af09a819b31596004aaf2b
Jere Stigell <jj.stigell@gmail.com>
2022-09-30 16:33:54 +0900

    Berry UI react template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+/*import './App.css';
 import Title from './components/Title';
 import NavBar from './components/NavBar';
 import { useSelector } from 'react-redux';
@@ -16,6 +16,41 @@ const App = () => {
       <Title />
       <NavBar user={user}/>
     </div>
+  );
+};
+
+export default App;
+*/
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline, StyledEngineProvider } from '@material-ui/core';
+
+// routing
+import Routes from './routes';
+
+// defaultTheme
+import theme from './themes';
+
+// project imports
+import NavigationScroll from './layout/NavigationScroll';
+
+//-----------------------|| APP ||-----------------------//
+
+const App = () => {
+  const customization = useSelector((state) => state.customization);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme(customization)}>
+        <CssBaseline />
+        <NavigationScroll>
+          <Routes />
+        </NavigationScroll>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 };
 
--- src/util/lngs.js
+++
@@ -1,4 +0,0 @@
-export const lngs = {
-  en: { nativeName: 'English' },
-  fi: { nativeName: 'Suomeksi' }
-};
