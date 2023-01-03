854d743d453e7ca50a8e4ef38f445ae92c03367d
Jere Stigell <jj.stigell@gmail.com>
2023-01-03 20:03:37 +0900

    session management

--- src/views/account/index.js
+++ src/views/account/index.js
@@ -2,9 +2,11 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { useLazyQuery } from '@apollo/client';
+import { useLazyQuery, useMutation } from '@apollo/client';
 
 // material-ui
+import DeleteIcon from '@material-ui/icons/Delete';
+import CancelIcon from '@material-ui/icons/Cancel';
 import {
   Typography,
   Button,
@@ -15,18 +17,29 @@ import {
   TableHead,
   TableRow,
   Paper,
-  IconButton
+  IconButton,
+  Modal,
+  Box
 } from '@material-ui/core';
-
-import DeleteIcon from '@material-ui/icons/Delete';
-
-
-
 
 // project imports
 import MainCard from '../../ui-component/cards/MainCard';
 import { SESSIONS } from '../../queries/queries';
-import { setSessions } from '../../store/accountReducer';
+import { DELETE_SESSION } from '../../queries/mutations';
+import { setSessions, removeSession, logOutAccount } from '../../store/accountReducer';
+import { formatDate } from '../../utils/formatDate';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 //==============================|| ACCOUNT PAGE ||==============================//
 
@@ -35,12 +48,20 @@ const AccountPage = () => {
   const { t } = useTranslation();
   const dispatcher = useDispatch();
   const [submitting, setSubmitting] = useState(false);
-  const [ fetSessions, { called, loading, data }] = useLazyQuery(SESSIONS);
+  let allowDelete = false;
+  const [modal, setModal] = useState(false);
+  const [ sessions, { called, loading, data } ] = useLazyQuery(SESSIONS);
+
+  const [ delSession, result ] = useMutation(DELETE_SESSION, {
+    onError: (error) => {
+      console.log(error);
+    }
+  });
 
   const fetchSessions = async () => {
     setSubmitting(true);
     try {
-      const res = await fetSessions();
+      const res = await sessions();
       if (res.errors) {
         const error = res.errors.graphQLErrors[0].extensions.code;
         console.log(error);
@@ -53,20 +74,34 @@ const AccountPage = () => {
     setSubmitting(false);
   };
 
-
   const deleteSession = async (id) => {
-
-    console.log(account.session);
-
-    console.log('deleteting session id:', id);
+    setSubmitting(true);
+    if (id === account.session && !allowDelete) {
+      setModal(true);
+    } else {
+      try {
+        const res = await delSession({
+          variables: {
+            sessionId: id
+          }
+        });
+        if (res.errors) {
+          const error = res.errors.graphQLErrors[0].extensions.code;
+          console.log(error);
+        } else if (res.data) {
+          console.log(res.data);
+          dispatcher(removeSession(res.data.deleteSession));
+          if (res.data.deleteSession === account.session) {
+            dispatcher(logOutAccount());
+          } else {
+            setSubmitting(false);
+          }
+        }
+      } catch(error) {
+        console.log('error:', error);
+      }
+    }
   };
-
-
-
-  //console.log(account.sessions);
-
-  //const x = [1,2,3,4,5,6,7,8];
-
 
   return (
     <>
@@ -78,22 +113,19 @@ const AccountPage = () => {
                 sunk in culpa qui officiate descent molls anim id est labours.
         </Typography>
       </MainCard>
-
       <h2>{t('accountPage.sessionsTitle')}</h2>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>{t('session.id')}</TableCell>
-              <TableCell align="right">{t('session.browser')}</TableCell>
-              <TableCell align="right">{t('session.os')}</TableCell>
-              <TableCell align="right">{t('session.device')}</TableCell>
-              <TableCell align="right">{t('session.createdAt')}</TableCell>
-              <TableCell align="right">{t('session.expireAt')}</TableCell>
-              <TableCell align="right">{t('session.delete')}</TableCell>
+              <TableCell>{t('session.browser')}</TableCell>
+              <TableCell>{t('session.device')}</TableCell>
+              <TableCell>{t('session.createdAt')}</TableCell>
+              <TableCell>{t('session.expireAt')}</TableCell>
+              <TableCell>{t('session.delete')}</TableCell>
             </TableRow>
           </TableHead>
-
           {!account.sessions ?
             <></>
             :
@@ -104,14 +136,14 @@ const AccountPage = () => {
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
-                    {session.id}
+                    {account.session === session.id ?
+                      <b>{session.id} {t('session.current')}</b> : session.id}
                   </TableCell>
-                  <TableCell align="right">{session.browser}</TableCell>
-                  <TableCell align="right">{session.os}</TableCell>
-                  <TableCell align="right">{session.device}</TableCell>
-                  <TableCell align="right">{session.createdAt}</TableCell>
-                  <TableCell align="right">{session.expireAt}</TableCell>
-                  <TableCell align="right">
+                  <TableCell>{session.browser}</TableCell>
+                  <TableCell>{session.device}</TableCell>
+                  <TableCell>{formatDate(session.createdAt)}</TableCell>
+                  <TableCell>{formatDate(session.expireAt)}</TableCell>
+                  <TableCell>
                     <IconButton onClick={() => deleteSession(session.id)}>
                       <DeleteIcon color="secondary" />
                     </IconButton>
@@ -120,7 +152,6 @@ const AccountPage = () => {
               ))}
             </TableBody>
           }
-
         </Table>
       </TableContainer>
       <Button
@@ -133,6 +164,35 @@ const AccountPage = () => {
       >
         {t('accountPage.fetchSessionsButton')}
       </Button>
+      <>
+        <Modal
+          open={modal}
+          onClose={() => setModal(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {t('misc.warning')}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {t('session.sessionWarning')}
+            </Typography>
+            <Button onClick={() => {
+              allowDelete = true;
+              deleteSession(account.session);
+            }} variant="outlined" startIcon={<DeleteIcon />}>
+              {t('buttonGeneral.delete')}
+            </Button>
+            <Button onClick={() => {
+              setModal(false);
+              setSubmitting(false);
+            }} variant="contained" endIcon={<CancelIcon />}>
+              {t('buttonGeneral.cancel')}
+            </Button>
+          </Box>
+        </Modal>
+      </>
     </>
   );
 };
