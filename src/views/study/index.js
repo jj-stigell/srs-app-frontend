435c7794cc532d10467065f4612440f9c2f6fb29
Jere Stigell <jj.stigell@gmail.com>
2023-01-04 23:04:29 +0900

    decks view

--- src/views/study/index.js
+++ src/views/study/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLazyQuery } from '@apollo/client';
 
 // material-ui
 import { Grid } from '@material-ui/core';
@@ -6,12 +9,39 @@ import { Grid } from '@material-ui/core';
 // project imports
 import DeckCard from './DeckCard';
 import { gridSpacing } from './../../store/constant';
+import { GET_DECKS } from '../../queries/queries';
+import { setDecks } from '../../store/deckReducer';
 
 //-----------------------|| STUDY DECKS ||-----------------------//
 
 const Dashboard = () => {
   const [isLoading, setLoading] = useState(true);
-  useEffect(() => {
+  const decks = useSelector(state => state.decks);
+  const userLanguage = useSelector(state => state.account.account.languageId);
+  const dispatcher = useDispatch();
+  const [ getDecks ] = useLazyQuery(GET_DECKS);
+
+  const fetchDecks = async () => {
+    try {
+      const res = await getDecks({
+        variables: {
+          date: new Date()
+        }
+      });
+      if (res.errors) {
+        const error = res.errors.graphQLErrors[0].extensions.code;
+        console.log(error);
+      } else if (res.data) {
+        dispatcher(setDecks(res.data.decks));
+      }
+    } catch(error) {
+      console.log('error:', error);
+    }
+  };
+
+  useEffect(async () => {
+    setLoading(true);
+    if (decks.length === 0) await fetchDecks();
     setLoading(false);
   }, []);
 
@@ -19,18 +49,11 @@ const Dashboard = () => {
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
         <Grid container spacing={gridSpacing}>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <DeckCard isLoading={isLoading} />
-          </Grid>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <DeckCard isLoading={isLoading} />
-          </Grid>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <DeckCard isLoading={isLoading} />
-          </Grid>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <DeckCard isLoading={isLoading} />
-          </Grid>
+          {decks.map(deck => (
+            <Grid key={deck.id} item lg={4} md={6} sm={6} xs={12}>
+              <DeckCard userLanguage={userLanguage} deck={deck} isLoading={isLoading} />
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Grid>
