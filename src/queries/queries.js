38fd8f9e2f3735bc5554f80202265829a1f75e2c
Jere Stigell <jj.stigell@gmail.com>
2023-01-04 21:42:41 +0900

    get decks

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -12,3 +12,38 @@ export const SESSIONS = gql`
     }
   }
 `;
+
+export const GET_DECKS = gql`
+query Decks($date: Date!) {
+  decks(date: $date) {
+    id
+    deckName
+    subscriberOnly
+    languageId
+    active
+    createdAt
+    updatedAt
+    deckTranslations {
+      id
+      languageId
+      title
+      description
+      active
+      createdAt
+      updatedAt
+    }
+    accountDeckSettings {
+      id
+      accountId
+      deckId
+      favorite
+      dueCards
+      reviewInterval
+      reviewsPerDay
+      newCardsPerDay
+      createdAt
+      updatedAt
+    }
+  }
+}
+`;
