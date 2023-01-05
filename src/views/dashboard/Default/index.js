466714cd798d468b2b30f44f3814b05b9484fceb
Jere Stigell <jj.stigell@gmail.com>
2023-01-05 14:52:34 +0900

    remove deck from front

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -5,7 +5,7 @@ import { Grid } from '@material-ui/core';
 
 // project imports
 import EarningCard from './EarningCard';
-import DeckCard from '../../study/DeckCard';
+//import DeckCard from '../../study/DeckCard';
 import PopularCard from './PopularCard';
 import TotalOrderLineChartCard from './TotalOrderLineChartCard';
 import TotalIncomeDarkCard from './TotalIncomeDarkCard';
@@ -21,12 +21,16 @@ const Dashboard = () => {
     setLoading(false);
   }, []);
 
+  /*
+            <DeckCard isLoading={isLoading} />
+*/
+
   return (
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
         <Grid container spacing={gridSpacing}>
           <Grid item lg={4} md={6} sm={6} xs={12}>
-            <DeckCard isLoading={isLoading} />
+
           </Grid>
           <Grid item lg={4} md={6} sm={6} xs={12}>
             <EarningCard isLoading={isLoading} />
