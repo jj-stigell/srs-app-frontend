6ee12edd27901f70ab3cee7f27dbc1783c9f83b9
Jere Stigell <jj.stigell@gmail.com>
2023-01-07 21:40:54 +0900

    mocdata for studyview

---
+++ src/views/study/StudyView/mockData.js
@@ -0,0 +1,172 @@
+export const mockData = [
+  {
+    id: 105,
+    cardType: 'WORD',
+    reviewType: 'RECALL',
+    createdAt: '2022-12-24T09:52:08.745Z',
+    updatedAt: '2022-12-24T09:52:08.745Z',
+    accountCard: null,
+    kanji: null,
+    word: {
+      id: 2,
+      word: '青',
+      jlptLevel: 5,
+      furigana: true,
+      reading: 'あお',
+      readingRomaji: 'ao',
+      createdAt: '2022-12-24T09:52:08.745Z',
+      updatedAt: '2022-12-24T09:52:08.745Z',
+      translation: {
+        translation: 'blue',
+        hint: 'placeholder',
+        description: 'placeholder',
+        createdAt: '2022-12-24T09:52:08.745Z',
+        updatedAt: '2022-12-24T09:52:08.745Z'
+      }
+    }
+  },
+  {
+    id: 105,
+    cardType: 'WORD',
+    reviewType: 'RECOGNISE',
+    createdAt: '2022-12-24T09:52:08.745Z',
+    updatedAt: '2022-12-24T09:52:08.745Z',
+    accountCard: null,
+    kanji: null,
+    word: {
+      id: 2,
+      word: '青',
+      jlptLevel: 5,
+      furigana: true,
+      reading: 'あお',
+      readingRomaji: 'ao',
+      createdAt: '2022-12-24T09:52:08.745Z',
+      updatedAt: '2022-12-24T09:52:08.745Z',
+      translation: {
+        translation: 'blue',
+        hint: 'placeholder',
+        description: 'placeholder',
+        createdAt: '2022-12-24T09:52:08.745Z',
+        updatedAt: '2022-12-24T09:52:08.745Z'
+      }
+    }
+  },
+  {
+    id: 2,
+    cardType: 'KANJI',
+    reviewType: 'RECALL',
+    createdAt: '2022-12-24T09:52:08.745Z',
+    updatedAt: '2022-12-24T09:52:08.745Z',
+    accountCard: null,
+    kanji: {
+      id: 2,
+      kanji: '上',
+      jlptLevel: 5,
+      onyomi: 'ジョウ、 ショウ、 シャン',
+      onyomiRomaji: 'jou, shou, shan',
+      kunyomi: 'うえ、 -うえ、 うわ-、 かみ、 あ.げる、 -あ.げる、 あ.がる、 -あ.がる、 あ.がり、 -あ.がり、 のぼ.る、 のぼ.り、 のぼ.せる、 のぼ.す、 たてまつ.る',
+      kunyomiRomaji: 'ue, -ue, uwa-, kami, a.geru, -a.geru, a.garu, -a.garu, a.gari, -a.gari, nobo.ru, nobo.ri, noba.seru, noba.su, tatematsu.ru',
+      strokeCount: 3,
+      createdAt: '2022-12-24T09:52:08.745Z',
+      updatedAt: '2022-12-24T09:52:08.745Z',
+      translation: {
+        keyword:'-',
+        story: '-',
+        hint:'-',
+        otherMeanings: null,
+        description: null,
+        createdAt: '2022-12-24T09:52:08.745Z',
+        updatedAt: '2022-12-24T09:52:08.745Z'
+      },
+      radicals: [
+        {
+          radical: '⼘',
+          reading: 'ぼく',
+          readingRomaji: null,
+          strokeCount: 2,
+          createdAt: '2022-12-24T09:52:08.745Z',
+          updatedAt: '2022-12-24T09:52:08.745Z',
+          translation: {
+            translation: 'oracle',
+            description: null,
+            createdAt: '2022-12-24T09:52:08.745Z',
+            updatedAt: '2022-12-24T09:52:08.745Z'
+          }
+        },
+        {
+          radical: '⼀',
+          reading: 'いち',
+          readingRomaji: null,
+          strokeCount: 1,
+          createdAt: '2022-12-24T09:52:08.745Z',
+          updatedAt: '2022-12-24T09:52:08.745Z',
+          translation: {
+            translation: 'one, horizontal stroke',
+            description: null,
+            createdAt: '2022-12-24T09:52:08.745Z',
+            updatedAt: '2022-12-24T09:52:08.745Z'
+          }
+        }
+      ]
+    }
+  },
+  {
+    id: 2,
+    cardType: 'KANJI',
+    reviewType: 'RECOGNISE',
+    createdAt: '2022-12-24T09:52:08.745Z',
+    updatedAt: '2022-12-24T09:52:08.745Z',
+    accountCard: null,
+    kanji: {
+      id: 2,
+      kanji: '上',
+      jlptLevel: 5,
+      onyomi: 'ジョウ、 ショウ、 シャン',
+      onyomiRomaji: 'jou, shou, shan',
+      kunyomi: 'うえ、 -うえ、 うわ-、 かみ、 あ.げる、 -あ.げる、 あ.がる、 -あ.がる、 あ.がり、 -あ.がり、 のぼ.る、 のぼ.り、 のぼ.せる、 のぼ.す、 たてまつ.る',
+      kunyomiRomaji: 'ue, -ue, uwa-, kami, a.geru, -a.geru, a.garu, -a.garu, a.gari, -a.gari, nobo.ru, nobo.ri, noba.seru, noba.su, tatematsu.ru',
+      strokeCount: 3,
+      createdAt: '2022-12-24T09:52:08.745Z',
+      updatedAt: '2022-12-24T09:52:08.745Z',
+      translation: {
+        keyword:'-',
+        story: '-',
+        hint:'-',
+        otherMeanings: null,
+        description: null,
+        createdAt: '2022-12-24T09:52:08.745Z',
+        updatedAt: '2022-12-24T09:52:08.745Z'
+      },
+      radicals: [
+        {
+          radical: '⼘',
+          reading: 'ぼく',
+          readingRomaji: null,
+          strokeCount: 2,
+          createdAt: '2022-12-24T09:52:08.745Z',
+          updatedAt: '2022-12-24T09:52:08.745Z',
+          translation: {
+            translation: 'oracle',
+            description: null,
+            createdAt: '2022-12-24T09:52:08.745Z',
+            updatedAt: '2022-12-24T09:52:08.745Z'
+          }
+        },
+        {
+          radical: '⼀',
+          reading: 'いち',
+          readingRomaji: null,
+          strokeCount: 1,
+          createdAt: '2022-12-24T09:52:08.745Z',
+          updatedAt: '2022-12-24T09:52:08.745Z',
+          translation: {
+            translation: 'one, horizontal stroke',
+            description: null,
+            createdAt: '2022-12-24T09:52:08.745Z',
+            updatedAt: '2022-12-24T09:52:08.745Z'
+          }
+        }
+      ]
+    }
+  }
+];
