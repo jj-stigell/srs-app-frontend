bd33bd5ff35e3a0054c135801fc3f31192740e56
Jere Stigell <jj.stigell@gmail.com>
2023-01-08 14:24:14 +0900

    information tab

--- src/views/study/StudyView/tabs/Information.js
+++ src/views/study/StudyView/tabs/Information.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
@@ -8,7 +9,7 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
   },
 }));
-const Information = () => {
+const Information = ({ card }) => {
   const classes = useStyles();
 
   const vocabulary = [
@@ -28,6 +29,52 @@ const Information = () => {
       translation: 'tarnss'
     },
   ];
+
+  /*
+    id: 105,
+    cardType: 'WORD',
+    reviewType: 'RECALL',
+    createdAt: '2022-12-24T09:52:08.745Z',
+    updatedAt: '2022-12-24T09:52:08.745Z',
+    accountCard: {
+      id: 1,
+      reviewCount: 65,
+      easyFactor: 2.5,
+      accountStory: 'this is my account story: cars are cool',
+      accountHint: 'My own hint: its too complex to make up a hint',
+      dueAt: '2021-03-08',
+      mature: false,
+      createdAt: '2022-12-24T09:52:08.745Z',
+      updatedAt: '2022-12-24T09:52:08.745Z'
+    },
+    word: {
+      id: 2,
+      word: '自動車',
+      jlptLevel: 5,
+      furigana: true,
+      reading: 'じどうしゃ',
+      readingRomaji: 'jidousha',
+      createdAt: '2022-12-24T09:52:08.745Z',
+      updatedAt: '2022-12-24T09:52:08.745Z',
+      translation: {
+        translation: 'card',
+        hint: 'this is the default hint for car',
+        story: 'Story for car word, default one',
+        description: 'description for the word car',
+        createdAt: '2022-12-24T09:52:08.745Z',
+        updatedAt: '2022-12-24T09:52:08.745Z'
+      }
+    }
+  */
+
+  /*
+  if (card.cardType === 'WORD') {
+
+  }
+*/
+
+
+
 
   return (
     <Table className={classes.root}>
