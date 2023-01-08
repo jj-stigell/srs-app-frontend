9279e673414fe8609398f4ae634c45dfd933ed82
Jere Stigell <jj.stigell@gmail.com>
2023-01-08 20:59:52 +0900

    study main view

--- src/views/study/StudyView/index.js
+++ src/views/study/StudyView/index.js
@@ -1,95 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-
-// material-ui
-import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
-import HelpIcon from '@material-ui/icons/Help';
-import { Tooltip, IconButton } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useLazyQuery } from '@apollo/client';
 
 // project imports
-import Tab from './tabs';
-
-const useStyles = makeStyles((theme) => ({
-  body: {
-    height: '100%',
-    backgroundColor: theme.palette.primary.light
-  },
-  partition: {
-    height: '50%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: '20px'
-  },
-  partitionSecond: {
-    height: '50%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: '20px'
-  },
-}));
+import Card from './card';
+import LoadingPage from './LoadingPage';
+import ReviewFinished from './ReviewFinished';
+import { setCards } from '../../../store/cardReducer';
+import { GET_CARDS_FROM_DECK } from '../../../queries/queries';
 
 const Study = () => {
-  const classes = useStyles();
-  const [revealed, setRevealed] = useState(false);
-  const [hintVisible, setHintVisible] = useState(false);
+  const dispatcher = useDispatch();
+  const cardStore = useSelector(state => state.cards);
+  const account = useSelector(state => state.account);
+  const [loading, setLoading] = useState(true);
+  const deckId = Number(useParams().id);
+  const [ loadCardsFromDeck ] = useLazyQuery(GET_CARDS_FROM_DECK);
 
-  const handleReschedule = (result) => {
+  useEffect(async () => {
+    const res = await loadCardsFromDeck({
+      variables: {
+        deckId: deckId,
+        date: new Date(),
+        newCards: true,
+        languageId: account.account.languageId
+      }
+    });
 
-    console.log('review result is:', result);
-  };
+    if (res?.errors) {
+      const error = res.errors.graphQLErrors[0].extensions.code;
+      console.log(res.errors);
+    } else if (res?.data) {
+      console.log(res.data.cardsFromDeck);
+      setLoading(false);
+      dispatcher(setCards(res.data.cardsFromDeck));
+    }
+  }, []);
 
-  return (
-    <Grid>
-      <div className={classes.body}>
-        {/* Upper half of the page */}
-        <div className={classes.partition}>
-          <h1>Car</h1>
-        </div>
-        <div className={classes.partition}>
-          { !hintVisible
-            ?
-            <Tooltip title="Show hint">
-              <IconButton
-                onClick={() => setHintVisible(true)} >
-                <HelpIcon />
-              </IconButton>
-            </Tooltip>
-            :
-            <h3>Draw just few lines</h3>
-          }
-        </div>
-        {/* Lower half of the page */}
-        {revealed && (
-          <div>
-            <hr />
-            <div className={classes.partition}>
-              <h1>自動車</h1>
-            </div>
-            <div className={classes.partitionSecond}>
-              <button onClick={() => handleReschedule('again')}>again</button>
-              <button onClick={() => handleReschedule('hard')}>hard</button>
-              <button onClick={() => handleReschedule('easy')}>easy</button>
-            </div>
-          </div>
-        )}
-        {/* Reveal button */}
-        {!revealed && (
-          <div className={classes.partitionSecond}>
-            <button onClick={() => setRevealed(true)}>show answer</button>
-          </div>
-        )}
-        {/* Menu toggle button */}
-        {revealed && (
-          <div>
-            <Tab />
-          </div>
-        )}
-      </div>
-    </Grid>
-  );
+  if (loading) return <LoadingPage />;
+
+  if (!cardStore.activeCard) return <ReviewFinished />;
+
+  return <Card />;
 };
 
 export default Study;
